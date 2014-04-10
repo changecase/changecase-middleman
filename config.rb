bc2eba0ef8d2bc524787e91d867a7e86d02b37c7
changecase <jeff.ja@gmail.com>
2014-04-10 10:16:52 -0700

    add portfolio of work

--- config.rb
+++ config.rb
@@ -6,6 +6,7 @@
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
+    blog.name = "blog"
     blog.prefix = "blog"
 
     blog.permalink = "{year}/{month}/{day}/{title}.html"
@@ -29,8 +30,35 @@ activate :blog do |blog|
   # blog.page_link = "page/{num}"
 end
 
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+    blog.name = "work"
+    blog.prefix = "work"
+
+    blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "work/{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+    blog.layout = "portfolio_layout"
+    blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
 page "/feed.xml", layout: false
 page "blog/*",    layout: :article_layout
+page "work/*",    layout: :portfolio_layout
 
 ###
 # Compass
@@ -92,13 +120,13 @@ set :markdown, fenced_code_blocks: true, smartypants: true
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
