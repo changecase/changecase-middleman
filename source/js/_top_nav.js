65be229ccad344bf3f37d871889311fd5d22b20a
changecase <jeff.ja@gmail.com>
2015-04-01 13:10:02 -0700

    Add a close icon to expanded navigation

--- source/js/_top_nav.js
+++ source/js/_top_nav.js
@@ -1,42 +1,30 @@
 $(document).ready(function(){
-  var breakpointDevice = "";
   var $_nav = $( '.header-container nav' );
   var $_main = $( '.main-container' );
+  var $_navIcon = $( '<i class="fa fa-times"></i>' );
 
-  $( window ).resize(function(){
-    var $_window = $( window );
+  // Collapse the navigation to start
+  $_nav.addClass( 'collapsed' );
 
-    // Tell the console which breakpoint we are at
-    if ( $_window.width() < 480 ){ 
-      if ( breakpointDevice != "default" ){
-        breakpointDevice = "default";
-        console.log( 'default size' );
-      }
-    } else if ( $_window.width() < 768 ){ 
-      if ( breakpointDevice != "smartphone" ) {
-        breakpointDevice = "smartphone";
-        console.log( 'smartphone size (480px)' );
-      }
-    } else if ( $_window.width() < 1140 ){
-      if ( breakpointDevice != "tablet" ) {
-        breakpointDevice = "tablet";
-        console.log( 'tablet size (768px)' );
-      }
-    } else {
-      if ( breakpointDevice != "desktop" ) {
-        breakpointDevice = "desktop";
-        console.log( 'desktop size (1140px)' );
-      }
+  // Expand the navigation when the hamburger is clicked
+  $_nav.on("click", function(){
+    $( this ).removeClass( 'collapsed' ).addClass( 'expanded' )
+      .before($_navIcon);
+  });
+
+  // Collapse the navigation when anything outside of the nav is clicked
+  $_main.on("click", function(){
+    if ( !$_nav.hasClass( 'collapsed' )){
+      $_nav.addClass( 'collapsed' ).removeClass( 'expanded' );
+      $_navIcon.detach();
     }
   });
 
-  $_nav.addClass( 'collapse' );
-  $_nav.on("click", function(){
-    $( this ).removeClass( 'collapse' );
-  });
-  $_main.on("click", function(){
-    if ( !$_nav.hasClass( 'collapse' )){
-      $_nav.addClass( 'collapse' );
+  // Collapse the navigation when the close icon is clicked
+  $_navIcon.on("click", function(){
+    if ( !$_nav.hasClass( 'collapsed' )){
+      $_nav.addClass( 'collapsed' ).removeClass( 'expanded' );
+      $_navIcon.detach();
     }
   });
 });
--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,4 @@
+//= require "utilities/_check_browser_width"
 //= require "_paralax"
 //= require "_top_nav"
 //= require "_feat_section"
