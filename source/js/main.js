f2105688b4b50f989f41eee6e3fe7667590ff2ab
changecase <jeff.ja@gmail.com>
2015-04-04 10:12:26 -0700

    Combine javascript into a single download
    Use sprockets to combine the typkit and modernizer javascripts into a
    single file download.
    
    Also, improve the presentation of code and pre blocks.

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,9 @@
+//= require "vendor/modernizr-2.6.2-respond-1.1.0.min.js"
+
 //= require "utilities/_check_browser_width"
 //= require "_paralax"
 //= require "_top_nav"
 //= require "_feat_section"
+//= require "_typekit"
+
+//= require "blog/_sharepoint_custom_tiles"
