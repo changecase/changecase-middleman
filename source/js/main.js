b0071ba8658968004b43c890794937275e4a84af
changecase <jeff.ja@gmail.com>
2015-04-09 13:30:28 -0700

    Convert browser width checker to module

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,8 @@
 //= require "vendor/jquery-1.10.1.min.js"
 //= require "vendor/modernizr-2.6.2-respond-1.1.0.min.js"
 
+//= require "utilities/_check_browser_width"
+
 //= require "_paralax"
 //= require "_top_nav"
 //= require "_feat_section"
@@ -10,6 +12,7 @@
 
 $( document ).ready(function() {
   // utilities
+  browserWidth.broadcast();
   topNav.toggle('.header-container nav');
 
   // index
--- source/js/utilities/_check_browser_width.js
+++ source/js/utilities/_check_browser_width.js
@@ -1,29 +1,41 @@
-function checkBrowserWidth() {
-  var breakpointDevice = "";
-  $( window ).resize(function(){
-    var $_window = $( window );
+(function (browserWidth) {
+  var devices, breakpointDevice;
 
-    // Tell the console which breakpoint we are at
-    if ( $_window.width() < 480 ){ 
-      if ( breakpointDevice != "default" ){
-        breakpointDevice = "default";
-        console.log( 'default size' );
-      }
-    } else if ( $_window.width() < 768 ){ 
-      if ( breakpointDevice != "smartphone" ) {
-        breakpointDevice = "smartphone";
-        console.log( 'smartphone size (480px)' );
-      }
-    } else if ( $_window.width() < 1140 ){
-      if ( breakpointDevice != "tablet" ) {
-        breakpointDevice = "tablet";
-        console.log( 'tablet size (768px)' );
-      }
-    } else {
-      if ( breakpointDevice != "desktop" ) {
-        breakpointDevice = "desktop";
-        console.log( 'desktop size (1140px)' );
+  devices = [
+    {device: "default",    min_width: 0},
+    {device: "smartphone", min_width: 480},
+    {device: "tablet",     min_width: 768},
+    {device: "desktop",    min_width: 1140}
+  ];
+
+  browserWidth.broadcast = function(){
+    for(i=0; i<devices.length; i++){
+      var $_browserWidth = $(window).width();
+      var thisWidth = devices[i].min_width;
+
+      if (typeof(devices[i + 1]) !== 'undefined') {
+        var nextWidth = devices[i + 1].min_width;
+
+        if ($_browserWidth < nextWidth) {
+          if ($_browserWidth >= thisWidth) {
+            if (breakpointDevice !== devices[i].device) {
+              console.log(devices[i].device + ' size (' + devices[i].min_width + 'px)');
+              breakpointDevice = devices[i].device;
+            }
+          }
+        }
+      } else {
+        if ($_browserWidth >= thisWidth) {
+          if (breakpointDevice !== devices[i].device) {
+            console.log(devices[i].device + ' size (' + devices[i].min_width + 'px)');
+            breakpointDevice = devices[i].device;
+          }
+        }
       }
     }
-  });
-}
+    $( window ).resize(function(){
+      browserWidth.broadcast();
+    });
+  };
+
+})(browserWidth = {})
