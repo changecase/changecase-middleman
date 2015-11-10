4348eb2840e06a727da0599516425dbf6e620aab
changecase <jeff.ja@gmail.com>
2015-11-10 14:10:56 -0800

    don't display hamburger menu on desktop

--- source/js/_top_nav.js
+++ source/js/_top_nav.js
@@ -2,35 +2,55 @@
   var $_main, $_navIcon;
 
   $_main = $( '.main-container' );
-  $_navIcon = $( '<i class="fa fa-times"></i>' );
+  $_navIcon = $( '<i class="fa"></i>' );
   
+  var addIcon = function(targetNav) {
+    targetNav.before( $_navIcon );
+  }
+
   var expand = function(targetNav) {
-    targetNav.removeClass( 'collapsed' ).addClass( 'expanded' )
-      .before($_navIcon);
+    $_navIcon.removeClass( 'open' ).addClass( 'close' );
+    targetNav.removeClass( 'collapsed' ).addClass( 'expanded' );
   }
 
   // Collapse the navigation when anything outside of the nav is clicked
-  var collapse = function (targetNav) {
-    if (!targetNav.hasClass('collapsed')){
-      targetNav.removeClass( 'expanded' ).addClass( 'collapsed' );
-      $_navIcon.detach();
-    }
+  var collapse = function(targetNav) {
+    $_navIcon.removeClass( 'close' ).addClass( 'open' );
+    targetNav.removeClass( 'expanded' ).addClass( 'collapsed' );
+  }
+
+  // Clear mobile classes on desktop and larger
+  var clear = function(targetNav) {
+    $_navIcon.removeClass( 'open close' );
+    targetNav.removeClass( 'collapsed expanded' );
   }
 
   mainNav.toggle = function (nav) {
     nav = $( nav );
+
+    
+    // Add navigation icon
+    addIcon( nav );
     
     // Collapse the navigation to start
-    nav.addClass( 'collapsed' );
+    if (!nav.hasClass('collapsed') && !nav.hasClass('expanded')) {
+      $_navIcon.addClass('open');
+      nav.addClass( 'collapsed' );
+    }
 
-    // Expand the navigation when the hamburger is clicked
-    nav.on(       'click', function(){ expand(nav); });
+    // When the hamburger/close icon is clicked
+    $_navIcon.on( 'click', function(){ 
+      if (nav.hasClass('collapsed')){
+        // Expand the navigation 
+        expand(nav); 
+      } else if (nav.hasClass('expanded')){
+        // Collapse the navigation 
+        collapse(nav);
+      }
+    });
 
     // Collapse the navigation when anything outside of the nav 
     // is clicked
     $_main.on(    'click', function(){ collapse(nav); });
-    
-    // Collapse the navigation when the close icon is clicked
-    $_navIcon.on( 'click', function(){ collapse(nav); });
   };
 })(this.topNav = {});
